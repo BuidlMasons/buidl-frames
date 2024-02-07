306e15c6d0d8c9dac5208dc9f6e56ab30e91abfa
Matthew Pereira <matthewdavidpereira@gmail.com>
2024-02-07 12:17:48 -0800

    send user to speedrun when image clicked

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,6 +1,32 @@
+import { getFrameMetadata } from "@coinbase/onchainkit";
+import type { Metadata } from "next";
+
 export const viewport = {
   width: "device-width",
   initialScale: 1.0,
+};
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "⚡ Start",
+    },
+  ],
+  image: `https://speedrunethereum.com/thumbnail.png`,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=0`,
+});
+
+export const metadata: Metadata = {
+  title: "Speed Run Ethereum",
+  description: "Learn how to build on Ethereum; the superpowers and the gotchas.",
+  openGraph: {
+    title: "Speed Run Ethereum",
+    description: "Learn how to build on Ethereum; the superpowers and the gotchas.",
+    images: [`https://speedrunethereum.com/thumbnail.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
