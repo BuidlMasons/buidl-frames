c2fdad50daf157a23382f7b66caddd2b9ea7915b
Matthew Pereira <matthewdavidpereira@gmail.com>
2024-02-06 20:41:07 -0800

    testing more buttons

--- packages/nextjs/app/api/frame/route.ts
+++ packages/nextjs/app/api/frame/route.ts
@@ -8,7 +8,17 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   const id: any = searchParams.get("id");
   const idAsNumber = parseInt(id);
 
-  const nextId = idAsNumber + 1;
+  const data = await req.json();
+  const buttonId = data.untrustedData.buttonIndex;
+
+  let slideNumber: number;
+  if (buttonId === 1) {
+    slideNumber = idAsNumber - 1;
+  } else if (buttonId === 2) {
+    slideNumber = idAsNumber + 1;
+  } else {
+    slideNumber = 0;
+  }
 
   if (idAsNumber === 7) {
     return new NextResponse(`<!DOCTYPE html><html><head>
@@ -19,7 +29,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="fc:frame:button:1:action" content="post_redirect" />
     <meta property="fc:frame:button:2" content="Buidl Guidl" />
     <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:button:3" content="Meet other builders" />
+    <meta property="fc:frame:button:3" content="Telegram" />
     <meta property="fc:frame:button:3:action" content="post_redirect" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
   </head></html>`);
@@ -28,8 +38,10 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeieshch2n347ttuuuyfkkemydwg7sxfsaemalrp3f5bujt4p3fxrei/${id}.png" />
-    <meta property="fc:frame:button:1" content="Next Page" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+    <meta property="fc:frame:button:1" content="Previous Challenge" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${slideNumber}" />
+    <meta property="fc:frame:button:2" content="Next Challenge" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${slideNumber}" />
   </head></html>`);
   }
 }
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -4,19 +4,19 @@ import type { Metadata } from "next";
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: "Begin",
+      label: "Start U+1F3C3",
     },
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeieshch2n347ttuuuyfkkemydwg7sxfsaemalrp3f5bujt4p3fxrei/0.png`,
+  image: `https://speedrunethereum.com/thumbnail.png`,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
 export const metadata: Metadata = {
   title: "Speed Run Ethereum",
-  description: "Learn how to build on Ethereum",
+  description: "Learn how to build on Ethereum; the superpowers and the gotchas.",
   openGraph: {
     title: "Speed Run Ethereum",
-    description: "Learn how to build on Ethereum",
+    description: "Learn how to build on Ethereum; the superpowers and the gotchas.",
     images: [
       `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeieshch2n347ttuuuyfkkemydwg7sxfsaemalrp3f5bujt4p3fxrei/0.png`,
     ],
