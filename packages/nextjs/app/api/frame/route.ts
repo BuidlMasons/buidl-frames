3acac9fd55e5fd475fb49b85092fd8b36f958afa
quizford <125616425+Astronaut828@users.noreply.github.com>
2024-02-08 00:14:12 +0000

    Update: IPFS images

--- packages/nextjs/app/api/frame/route.ts
+++ packages/nextjs/app/api/frame/route.ts
@@ -14,7 +14,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 9</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeihrup7ar2zp46zupnvvlqjud6iv5jsbgcnv27jxpnjv6yjny7aaq4/9.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeiefo4lpojgsq6n5trcfq45r6xw6v6u3mhy2gtvr66qmdxl3zfwdaa/9.png" />
     <meta property="fc:frame:button:1" content="🏃 SpeedRun" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
     <meta property="fc:frame:button:2" content="🏰 BuidlGuidl" />
@@ -27,7 +27,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeihrup7ar2zp46zupnvvlqjud6iv5jsbgcnv27jxpnjv6yjny7aaq4/${id}.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeiefo4lpojgsq6n5trcfq45r6xw6v6u3mhy2gtvr66qmdxl3zfwdaa/${id}.png" />
     <meta property="fc:frame:button:1" content="Next Challenge ⏭️" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
   </head></html>`);
--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -12,7 +12,7 @@ const frameMetadata = getFrameMetadata({
       label: "⚡ Start",
     },
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeihrup7ar2zp46zupnvvlqjud6iv5jsbgcnv27jxpnjv6yjny7aaq4/0.png`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeiefo4lpojgsq6n5trcfq45r6xw6v6u3mhy2gtvr66qmdxl3zfwdaa/0.png`,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     title: "Speed Run Ethereum",
     description: "Learn how to build on Ethereum; the superpowers and the gotchas.",
     images: [
-      `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeihrup7ar2zp46zupnvvlqjud6iv5jsbgcnv27jxpnjv6yjny7aaq4/0.png`,
+      `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeiefo4lpojgsq6n5trcfq45r6xw6v6u3mhy2gtvr66qmdxl3zfwdaa/0.png`,
     ],
   },
   other: {
